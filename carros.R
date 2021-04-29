22e313a70419976f68188ad8c9f202a67226be53
Alan Ensina <alanvinicius.ce@gmail.com>
2021-04-29 16:18:20 -0300

    WIP on R code

--- carros.R
+++ carros.R
@@ -1,43 +1,78 @@
 # Leitura dos dados
-dados <- read.table("/home/alan/Downloads/carros.txt", # modificar o caminho
+library(ggplot2)
+dados <- read.table("/home/alan/Github/INE5649-StatisticalPredictionTechniques/carros.txt", # modificar o caminho
                     stringsAsFactors = T, # strings são fatores
                     header=T) # primeira linha do arquivo são os rótulos das variáveis
 head(dados) # imprimir início do data frame
 
-# Gráficos 2 a 2 das variáveis em estudo (cada variável explicativa versus variável resposta)
-library(ggplot2)
-ggplot(data=dados, 
-       aes(x=ano,y=valor))+
+# Histograma da variável resposta
+ggplot(data=dados, aes(valor)) + 
+  geom_histogram()
+
+# Shapiro test de todas as variáveis
+shapiro.test(dados$valor)
+shapiro.test(dados$ano)
+shapiro.test(dados$quilometragem)
+shapiro.test(dados$motor)
+shapiro.test(dados$modelo)
+
+# Gráficos das variáveis em estudo (cada variável explicativa versus variável resposta)
+# Análise regressão linear simples entre Valor e Ano
+g1 = ggplot(data=dados, 
+            aes(x=ano,y=valor))+
   geom_point() +
   labs(x = 'Ano', y = 'Valor (reais)') +
   theme_minimal()
 
-ggplot(data=dados, 
-       aes(x=quilometragem,y=valor))+
+modelo1 = lm(dados$valor~dados$ano)
+coef(modelo1)
+g1 + geom_smooth(method='lm', se=F)
+summary(modelo1)
+
+# Análise regressão linear simples entre Valor e Ano
+g2 = ggplot(data=dados, 
+            aes(x=quilometragem,y=valor))+
   geom_point()+
   labs(x = 'Quilometragem', y = 'Valor (reais)') +
   theme_minimal()
 
+modelo2 = lm(dados$valor~dados$ano)
+coef(modelo2)
+g2 + geom_smooth(method='lm', se=F)
+summary(modelo2)
+
+# Leitura das variáveis indicadoras
 dados$motor <- factor(dados$motor)
 dados$modelo <- factor(dados$modelo)
 
+# Criação do gráfico boxplot entre Valor e Motor
 ggplot(data=dados, 
        aes(x=motor,y=valor, fill = motor))+
   geom_boxplot(show.legend = F) +
   labs(x="Motor", y = "Valor") +
   theme_minimal()
 
+# Análise regressão linear simples entre Valor e Motor
+modelo3 = lm(dados$valor~dados$motor)
+coef(modelo3)
+anova(modelo3)
+
+# Criação do gráfico boxplot entre Valor e Modelo
 ggplot(data=dados, 
        aes(x=modelo,y=valor, fill = modelo))+
   geom_boxplot(show.legend = F) +
   labs(x="Modelo", y = "Valor") +
   theme_minimal()
 
+# Análise regressão linear simples entre Valor e Motor
+modelo4 = lm(dados$valor~dados$modelo)
+coef(modelo4)
+anova(modelo4)
+
 # Ajuste do modelo de regressão linear múltipla
-modelo <- lm(valor ~ ano + quilometragem + motor + modelo, # após o '~' adicionamos todas as variáveis explicativas separadas por '+'
-             data=dados) # nome do objeto onde encontram-se as variáveis
-coef(modelo) # imprimir coeficientes do modelo
+modeloX <- lm(valor ~ ano + quilometragem + motor + modelo, data=dados) 
+coef(modeloX) # imprimir coeficientes do modelo
 
-summary(modelo)
-
-anova(modelo)
+summary(modeloX)
+confint(modeloX)
+anova(modeloX)
